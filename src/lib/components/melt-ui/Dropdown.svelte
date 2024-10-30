25c452069d3c403c70705aa18a5e79fb601558cc
Kyle Nakamura <knakamura13dev@gmail.com>
2024-10-29 18:08:16 -0700

    Finish demo melt dropdown component

--- src/lib/components/melt-ui/Dropdown.svelte
+++ src/lib/components/melt-ui/Dropdown.svelte
@@ -2,16 +2,18 @@
     /**
      * Dropdown.svelte
      *
-     * This Melt component required multiple pre-requisite steps:
+     * This Melt component implementation required multiple pre-requisite steps:
      *  1. Installed '@tailwindcss/typography'
-     *  2. Installed lucide-svelte
+     *  2. Installed 'lucide-svelte' icon library
      *  3. Replaced tailwind.config.js with config found in the example
      *  4. Once the example was implemented, I made the following additional customizations:
      *      - Removed the sub-menu functionality
+     *      - Removed the menu arrow since it was incompatible with overflow-scroll
      *      - Made the menu scrollable with a constrained max height
      *      - Improved styling for disabled menu items
      *      - Fixed invisible separators with weird pt-[1px] workaround
-     *      - Removed the menu arrow since it was incompatible with overflow-scroll
+     *      - Converted Tailwind to SCSS
+     *      - Implemented custom defaults for menu options
      *
      * Source: https://www.melt-ui.com/docs/builders/dropdown-menu
      */
@@ -20,26 +22,34 @@
     import { AlignJustify, Check } from 'lucide-svelte';
     import { fly } from 'svelte/transition';
 
+    // Writable stores for checkbox items
     const settingsSync = writable(true);
     const hideMeltUI = writable(false);
 
+    // Dropdown menu configuration
     const {
         elements: { trigger, menu, item, separator },
         builders: { createCheckboxItem },
         states: { open }
     } = createDropdownMenu({
+        loop: false,
         forceVisible: true,
-        loop: true
+        closeOnItemClick: false,
+        positioning: {
+            placement: 'bottom'
+        }
     });
 
+    // Dropdown item configuration: setting sync
     const {
-        elements: { checkboxItem }
+        elements: { checkboxItem: settingsCheckboxItem }
     } = createCheckboxItem({
         checked: settingsSync
     });
 
+    // Dropdown item configuration: hide Melt UI
     const {
-        elements: { checkboxItem: checkboxItemA }
+        elements: { checkboxItem: hideMeltCheckboxItem }
     } = createCheckboxItem({
         checked: hideMeltUI
     });
@@ -57,7 +67,7 @@
 
         <div class="separator" use:melt={$separator} />
 
-        <div class="item" use:melt={$checkboxItem}>
+        <div class="item" use:melt={$settingsCheckboxItem}>
             <div class="check">
                 {#if $settingsSync}
                     <Check class="size-4" />
@@ -68,7 +78,7 @@
 
         <div use:melt={$separator} class="separator" />
 
-        <div class="item" use:melt={$checkboxItemA}>
+        <div class="item" use:melt={$hideMeltCheckboxItem}>
             <div class="check">
                 {#if $hideMeltUI}
                     <Check class="size-4" />
@@ -92,57 +102,121 @@
     </div>
 {/if}
 
-<style lang="postcss">
+<style lang="scss">
+    $magnum-900: #793a15;
+    $magnum-500: #f38d1c;
+    $magnum-400: #f7b155;
+    $magnum-200: #fce0ac;
+    $neutral-300: #d4d4d4;
+
     .menu {
-        @apply z-40 flex max-h-[300px] min-w-[220px] flex-col shadow-lg;
-        @apply rounded-md bg-white p-1 shadow-neutral-900/30;
-        @apply ring-0 overflow-y-scroll !important;
+        z-index: 40;
+        display: flex;
+        overflow-y: auto;
+        flex-direction: column;
+        min-width: 220px;
+        max-height: 300px;
+        padding: 0.25rem; /* 4px */
+        border-radius: 0.375rem; /* 6px */
+        outline: 0;
+        background-color: white;
+        box-shadow:
+            0 10px 15px -3px rgba(23, 23, 23, 0.3),
+            0 4px 6px -2px rgba(23, 23, 23, 0.15);
     }
 
     .item {
-        @apply relative h-6 min-h-[24px] select-none cursor-pointer rounded-sm pl-6 pr-1;
-        @apply z-40 text-magnum-900 outline-none;
-        @apply hover:text-magnum-900;
-        @apply flex items-center text-sm leading-none;
-        @apply ring-0 !important;
-    }
+        font-size: 0.875rem; /* 14px */
+        font-weight: normal;
+        line-height: 1;
+        position: relative;
+        z-index: 40;
+        display: flex;
+        align-items: center;
+        height: 1.5rem; /* 24px */
+        min-height: 24px;
+        padding-right: 0.25rem; /* 4px */
+        padding-left: 1.5rem; /* 24px */
+        cursor: pointer;
+        user-select: none;
+        color: $magnum-900;
+        border-radius: 0.125rem; /* 2px */
+        outline: none;
+        background-color: transparent;
 
-    /* Apply bg-magnum-200 on hover only when not disabled */
-    .item:not([data-disabled]):hover {
-        @apply bg-magnum-200;
-    }
+        &:hover {
+            color: $magnum-900;
+        }
 
-    /* Styles for highlighted items */
-    .item[data-highlighted] {
-        @apply bg-magnum-200 text-magnum-900;
-    }
+        &:not([data-disabled]):hover {
+            background-color: $magnum-200;
+        }
 
-    /* Styles for disabled items */
-    .item[data-disabled],
-    .item[data-disabled]:hover {
-        @apply bg-transparent text-neutral-300 cursor-default;
+        &[data-highlighted] {
+            color: $magnum-900;
+            background-color: $magnum-200;
+        }
+
+        &[data-disabled],
+        &[data-disabled]:hover {
+            cursor: default;
+            color: $neutral-300;
+            background-color: transparent;
+        }
     }
 
     .trigger {
-        @apply inline-flex h-9 w-9 items-center justify-center rounded-full bg-white;
-        @apply text-magnum-900 transition-colors hover:bg-white/90;
-        @apply data-[highlighted]:ring-magnum-400 data-[highlighted]:ring-offset-2 !important;
-        @apply p-0 text-sm font-medium data-[highlighted]:outline-none;
+        font-size: 0.875rem; /* 14px */
+        font-weight: 500;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        width: 2.25rem; /* 36px */
+        height: 2.25rem; /* 36px */
+        margin: 0;
+        padding: 0;
+        transition:
+            color 150ms ease-in-out,
+            background-color 150ms ease-in-out;
+        color: $magnum-900;
+        border-radius: 9999px;
+        background-color: white;
+        box-shadow:
+            0 1px 6px -3px rgba(23, 23, 23, 0.1),
+            0 1px 6px -2px rgba(23, 23, 23, 0.1);
+
+        &:hover {
+            background-color: $magnum-200;
+        }
+
+        &[data-highlighted] {
+            outline: none;
+            outline-offset: 2px;
+            box-shadow: 0 0 0 2px $magnum-200;
+        }
     }
+
     .check {
-        @apply absolute left-2 top-1/2 text-magnum-500;
-        translate: 0 calc(-50% + 1px);
+        position: absolute;
+        top: 50%;
+        left: 0;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        width: 1.5rem; /* 24px */
+        transform: translateY(calc(-50% + 1px));
+        color: $magnum-500;
     }
 
     .separator {
-        @apply m-[5px] h-[1px] pt-[1px] bg-magnum-200;
+        height: 1px;
+        margin: 5px;
+        padding-top: 1px;
+        background-color: $magnum-200;
     }
 
     .rightSlot {
-        @apply ml-auto pl-5;
-    }
-
-    .check {
-        @apply absolute left-0 inline-flex w-6 items-center justify-center;
+        margin-left: auto;
+        padding-left: 1.25rem; /* 20px */
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
     import Dropdown from '$lib/components/melt-ui/Dropdown.svelte';
 </script>
 
-<div class="page" id="home">
+<div class="page prose" id="home">
     <section class="intro">
-        <h1 class="text-2xl font-medium">Svelte Component Sandbox</h1>
+        <h1 class="prose-h1">Svelte Component Sandbox</h1>
     </section>
 
     <section class="melt-ui">
-        <h2 class="text-xl font-medium">Melt UI</h2>
-        <p class="font-bold">Dropdown Menu</p>
+        <h2 class="prose-h2">Melt UI</h2>
+        <p class="prose-strong">Dropdown Menu</p>
         <Dropdown />
     </section>
 </div>
